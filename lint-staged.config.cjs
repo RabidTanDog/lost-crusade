a964f1bc3bf5123c6ee7e62374cb48425462494c
Mike Pratt <rabidtandog@gmail.com>
2023-08-29 06:38:20 +0000

    build: prettier, husky, and conventional commits

---
+++ lint-staged.config.cjs
@@ -0,0 +1,3 @@
+module.exports = {
+  "*.{ts,tsx,js,jsx,json,scss,css,md,astro}": ["prettier --write"],
+};
---
+++ prettier.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+    {
+      files: ["*.json", "*.yaml", "*.yml"],
+      options: {
+        singleQuote: false,
+        trailingComma: "none",
+      },
+    },
+  ],
+};
