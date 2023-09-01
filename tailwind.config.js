8bb1fe4e2345515d70791b9448f24751fc7eabe0
Mike Pratt <rabidtandog@gmail.com>
2023-09-01 03:15:37 +0000

    feat: show list of mechanics

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        highlight: [
+          "0 0px 7px rgba(200, 200, 200, 0.35)",
+          "0 0px 25px rgba(200, 200, 200, 0.2)",
+        ],
+      },
+    },
   },
-  plugins: [],
 };
